e33cca8a5be67297debde7edfd8c94498444b5ff
Vlad Fedorenko <vfedo92@gmail.com>
2017-02-23 13:42:34 +0300

    Changed author pattern

--- app/src/main/java/by/vfedorenko/githubmanager/presentation/repositories/ReposViewModel.kt
+++ app/src/main/java/by/vfedorenko/githubmanager/presentation/repositories/ReposViewModel.kt
@@ -8,8 +8,8 @@ import by.vfedorenko.githubmanager.presentation.login.LoginActivity
 import javax.inject.Inject
 
 /**
- * Created by Vlad Fedorenko <vfedo92@gmail.com> on 26.01.17.
- */
+* @author Vlad Fedorenko <vfedo92@gmail.com> on 26.01.17.
+*/
 @RepoScope
 class ReposViewModel @Inject constructor(authUserManager: AuthUserManager,
                                          private val navigationManager: NavigationManager) {
